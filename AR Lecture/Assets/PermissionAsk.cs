7b8ae2f5d75a5af71dfe0f2cccb26d2ffeda5c92
sherlore <sherlore@gmail.com>
2019-12-11 23:09:38 +0800

    Fix black screen
    Update Vuforia to 8.1.10
    Add permission ask of camera

---
+++ AR Lecture/Assets/PermissionAsk.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if PLATFORM_ANDROID
+using UnityEngine.Android;
+#endif
+
+public class PermissionAsk : MonoBehaviour
+{
+    void Start ()
+    {
+        #if PLATFORM_ANDROID
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            Permission.RequestUserPermission(Permission.Camera);
+        }
+        #endif
+    }
+
+}
